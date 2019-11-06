2c77a2d06e357ad5610e50d2c6ef66aad2bb56ee
Symeonw <Symeonwhite16@gmail.com>
2019-11-06 14:34:12 -0600

    0.0.9

--- explore.py
+++ explore.py
@@ -3,5 +3,12 @@ import pandas as pd
 import pandas_profiling
 import prep
 import seaborn as sns
-df = prep_df()
-sns.pairplot(df)
+import pandas_profiling
+df = prep.prep_df()
+df_2 = df.drop(columns= ["logerror"])
+explore_df = pd.Series(df_2.corrwith(df["logerror"]))
+explore_df.nlargest(n=5)
+explore_df.nsmallest(n=5)
+profile = df.profile_report()
+rejected_variables = profile.get_rejected_variables(threshold=0.9)
+profile
