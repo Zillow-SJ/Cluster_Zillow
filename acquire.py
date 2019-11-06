5b323fb34be0beffdf1541579f5b8c6edae51121
JefferyRoeder <jefferyroeder@gmail.com>
2019-11-06 11:08:16 -0600

    Initial Exploration and acquire.py added

---
+++ acquire.py
@@ -0,0 +1,40 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import env
+import seaborn as sns
+
+url = env.get_db_url('zillow')
+#
+def wrangle_zillow():
+    df = pd.read_sql("""
+
+SELECT 
+p17.transactiondate,p.id,p.bathroomcnt as bathrooms,p.bedroomcnt as bedrooms, p.calculatedfinishedsquarefeet as sqft, p.taxvaluedollarcnt as tax_value, `architecturalstyledesc`,a.*, p.*,c.*
+FROM propertylandusetype pl
+JOIN
+properties_2017 p ON p.propertylandusetypeid = pl.propertylandusetypeid
+JOIN
+predictions_2017 p17 ON p17.id = p.id
+LEFT JOIN architecturalstyletype a USING(architecturalstyletypeid)
+LEFT JOIN typeconstructiontype c USING(typeconstructiontypeid)
+
+
+
+WHERE 
+pl.propertylandusedesc LIKE '%%Single Family%%'
+AND 
+p.calculatedfinishedsquarefeet IS NOT NULL
+and
+p.bedroomcnt > 0
+and 
+p.bathroomcnt > 0
+and
+p.taxvaluedollarcnt > 0
+
+"""
+,url)
+    return df
